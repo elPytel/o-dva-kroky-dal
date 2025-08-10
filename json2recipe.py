5a450e1ce9414928dba573ff6f2fdfe95893c8b6
elPytel <jaroslav.korner1@gmail.com>
2025-08-10 23:25:01 +0200

    Refactor content formatting in recipe conversion process

--- json2recipe.py
+++ json2recipe.py
@@ -21,14 +21,24 @@ title: {title}
 {content}
 """
 
+def format_content(note):
+    content = note['content']
+    # za ":" musí být \n
+    content = content.replace(":", ":\n")
+    # před ingrediencemi musí být prázdný řádek
+    content = content.replace("Ingredience:", "\n# Ingredience:")
+    content = content.replace("Postup:", "\n# Postup:")
+    return content
+
 # from notes in list to .md files in _recipes
 for note in data:
     if 'Recepty' not in note['tags']:
         continue
 
     filename = os.path.join(RECIPES_FOLDER, f"{note['id']}.md")
+    content = format_content(note)
     with open(filename, "w") as f:
-        f.write(recipe_template.format(title=note['title'], content=note['content']))
+        f.write(recipe_template.format(title=note['title'], content=content))
 
 print(f"Converted {len(data)} notes to recipes in {RECIPES_FOLDER}.")
 print("Done.")
