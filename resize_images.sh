62b521db330ea65ef12dbad545da7548f2f15690
elPytel <jaroslav.korner1@gmail.com>
2025-08-15 22:18:46 +0200

    upravená script

--- resize_images.sh
+++ resize_images.sh
@@ -5,20 +5,26 @@ PHOTO_DIR="./assets/fotky/"
 
 # Maximální velikost souboru (500 KB = 500000 bytes)
 MAX_SIZE=500000
+RESOLUTION="1920x1920"
 
 # Projít všechny JPG soubory ve složce
 for img in $(ls "$PHOTO_DIR"*.jpg); do
-  # Zjistit velikost souboru v bytech
-  FILE_SIZE=$(stat -c %s "$img")
+    # Zjistit velikost souboru v bytech
+    FILE_SIZE=$(stat -c %s "$img")
 
-  # Pokud je obrázek větší než 500 KB
-  if [ $FILE_SIZE -gt $MAX_SIZE ]; then
-    # Změnit velikost obrázku
-    mogrify -resize 1920x1920\> -quality 75 -strip "$img"
-    echo "Obrázek $img byl zmenšen na velikost menší než 500 KB."
-  else
-    echo "Obrázek $img je již menší než 500 KB, nic se neděje."
-  fi
+
+    # Pokud je obrázek větší než 500 KB
+    if [ $FILE_SIZE -gt $MAX_SIZE ]; then
+        # Změnit velikost obrázku
+        while [ $FILE_SIZE -gt $MAX_SIZE ]; do
+            # Změnit velikost obrázku
+            mogrify -resize $RESOLUTION\> -quality 75 -strip "$img"
+            FILE_SIZE=$(stat -c %s "$img")
+        done
+        echo "Obrázek $img byl zmenšen na velikost $FILE_SIZE bytes."
+    else
+        echo "Obrázek $img je již menší než 500 KB, nic se neděje."
+    fi
 done
 
 echo -e "Done."
