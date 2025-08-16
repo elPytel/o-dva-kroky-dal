e46b223a8108337da97ae70758c4c9b25f7f68c7
elPytel <jaroslav.korner1@gmail.com>
2025-08-16 19:52:22 +0200

    upravenáýscript

--- resize_images.sh
+++ resize_images.sh
@@ -1,11 +1,38 @@
 #!/bin/bash
 
 # Cesta ke složce s fotkami
-PHOTO_DIR="./assets/fotky/"
+PHOTO_DEST_DIR="./assets/fotky/"
+PHOTO_DEST_DIR="./big_photos/"
+PHOTO_SOURCE_DIR="./big_photos/"
 
 # Maximální velikost souboru (500 KB = 500000 bytes)
 MAX_SIZE=500000
 RESOLUTION="1920x1920"
+RESOLUTION="1280x1280"
+COMPRESION="75"
+
+function Help() {
+	echo "Compresing images in source dir."
+	echo
+	echo "Usage: $0 [options] [arguments]"
+	echo
+	echo "Options:"
+	echo "-s source         source foulcer"
+	echo "-c compresion     Compresion quality, defoult 75"
+	echo "-h                Description of the script operation"
+}
+
+while getopts s:d:r:c:h flag
+do
+	case "${flag}" in
+        s) PHOTO_SOURCE_DIR=${OPTARG};;
+		d) PHOTO_DEST_DIR=${OPTARG};;
+		r) RESOLUTION=${OPTARG};;
+        c) COMPRESION=${OPTARG};;
+		h) Help
+		   exit;;
+	esac
+done
 
 # Projít všechny JPG soubory ve složce
 for img in $(ls "$PHOTO_DIR"*.jpg); do
